b484b5747c2f54490e1224b9da5491765a011901
Gonzalo Chumillas <gchumillas@email.com>
2015-11-21 22:02:04 +0100

    make dist files

--- make-dist.sh
+++ make-dist.sh
@@ -7,9 +7,13 @@ rm -rf dist/*
 
 # compresses JavaScript files
 cat \
-    src/jquery.sp-regex.js \
+    src/jquery.sp-context-menu.js \
 | uglifyjs \
     --compress \
     --mangle \
     --preamble "/*! jQuery.spRegex v0.1.0 | Copyright (c) 2015 Gonzalo Chumillas | https://github.com/soloproyectos-js/jquery.sp-context-menu/blob/master/README.md */" \
     -o dist/jquery.sp-context-menu.min.js
+# compresses CSS files
+cat \
+    src/jquery.sp-context-menu.css \
+| uglifycss > dist/jquery.sp-context-menu.min.css
